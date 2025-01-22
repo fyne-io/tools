11b5977bc0e31c863db0ff59a202cc39a6574ca6
Jacalz <jacalz@tutanota.com>
2025-01-22 11:19:46 +0100

    Use go:embed for templates and files bundled into cmd/fyne

--- cmd/fyne/internal/templates/templates.go
+++ cmd/fyne/internal/templates/templates.go
@@ -2,57 +2,55 @@ package templates
 
 import "text/template"
 
-//go:generate go run ../.. bundle -package templates -o bundled.go data
-
 var (
 	// MakefileUNIX is the template for the makefile on UNIX systems like Linux and BSD
-	MakefileUNIX = template.Must(template.New("Makefile").Parse(string(resourceMakefile.StaticContent)))
+	MakefileUNIX = template.Must(template.New("Makefile").Parse(makefile))
 
 	// DesktopFileUNIX is the template the desktop file on UNIX systems like Linux and BSD
-	DesktopFileUNIX = template.Must(template.New("DesktopFile").Parse(string(resourceAppDesktop.StaticContent)))
+	DesktopFileUNIX = template.Must(template.New("DesktopFile").Parse(desktopEntry))
 
 	// EntitlementsDarwin is a plist file that lists build entitlements for darwin releases
-	EntitlementsDarwin = template.Must(template.New("Entitlements").Parse(string(resourceEntitlementsDarwinPlist.StaticContent)))
+	EntitlementsDarwin = template.Must(template.New("Entitlements").Parse(entitlementsDarwinPlist))
 
 	// EntitlementsDarwinMobile is a plist file that lists build entitlements for iOS releases
-	EntitlementsDarwinMobile = template.Must(template.New("EntitlementsMobile").Parse(string(resourceEntitlementsIosPlist.StaticContent)))
+	EntitlementsDarwinMobile = template.Must(template.New("EntitlementsMobile").Parse(entitlementsIosPlist))
 
 	// FyneMetadataInit is the metadata injecting file for fyne metadata
-	FyneMetadataInit = template.Must(template.New("fyne_metadata_init.got").Parse(string(resourceFynemetadatainitGot.StaticContent)))
+	FyneMetadataInit = template.Must(template.New("fyne_metadata_init.got").Parse(fyneMetadataInit))
 
 	// FynePprofInit is the file injected to support pprof
-	FynePprofInit = template.Must(template.New("fyne_pprof_init.got").Parse(string(resourceFynepprofGot.StaticContent)))
+	FynePprofInit = template.Must(template.New("fyne_pprof_init.got").Parse(fynePprof))
 
 	// ManifestAndroid is the default manifest for building an Android application
-	ManifestAndroid = template.Must(template.New("AndroidManifest").Parse(string(resourceAndroidManifestXml.StaticContent)))
+	ManifestAndroid = template.Must(template.New("AndroidManifest").Parse(androidManifestXML))
 
 	// ManifestWindows is the manifest file for windows packaging
-	ManifestWindows = template.Must(template.New("Manifest").Parse(string(resourceAppManifest.StaticContent)))
+	ManifestWindows = template.Must(template.New("Manifest").Parse(appManifest))
 
 	// AppxManifestWindows is the manifest file for windows packaging
-	AppxManifestWindows = template.Must(template.New("ReleaseManifest").Parse(string(resourceAppxmanifestXML.StaticContent)))
+	AppxManifestWindows = template.Must(template.New("ReleaseManifest").Parse(appxManifestXML))
 
 	// InfoPlistDarwin is the manifest file for darwin packaging
-	InfoPlistDarwin = template.Must(template.New("InfoPlist").Parse(string(resourceInfoPlist.StaticContent)))
+	InfoPlistDarwin = template.Must(template.New("InfoPlist").Parse(infoPlist))
 
 	// XCAssetsDarwin is the Contents.json file for darwin xcassets bundle
-	XCAssetsDarwin = template.Must(template.New("XCAssets").Parse(string(resourceXcassetsJSON.StaticContent)))
+	XCAssetsDarwin = template.Must(template.New("XCAssets").Parse(xcassetsJSON))
 
 	// IndexHTML is the index.html used to serve web package
-	IndexHTML = template.Must(template.New("index.html").Parse(string(resourceIndexHtml.StaticContent)))
-
-	// WebGLDebugJs is the content of https://raw.githubusercontent.com/KhronosGroup/WebGLDeveloperTools/b42e702487d02d5278814e0fe2e2888d234893e6/src/debug/webgl-debug.js
-	WebGLDebugJs = resourceWebglDebugJs.StaticContent
-
-	// SpinnerLight is a spinning gif of Fyne logo with a light background
-	SpinnerLight = resourceSpinnerlightGif.StaticContent
-
-	// CSSLight is a CSS that define color for element on the web splash screen following the light theme
-	CSSLight = resourceLightCss.StaticContent
-
-	// SpinnerDark is a spinning gif of Fyne logo with a dark background
-	SpinnerDark = resourceSpinnerdarkGif.StaticContent
+	IndexHTML = template.Must(template.New("index.html").Parse(indexHTML))
 
 	// CSSDark is a CSS that define color for element on the web splash screen following the dark theme
-	CSSDark = resourceDarkCss.StaticContent
+	CSSDark = darkCSS
+
+	// CSSLight is a CSS that define color for element on the web splash screen following the light theme
+	CSSLight = lightCSS
+
+	// SpinnerDark is a spinning gif of Fyne logo with a dark background
+	SpinnerDark = spinnerDarkGIF
+
+	// SpinnerLight is a spinning gif of Fyne logo with a light background
+	SpinnerLight = spinnerLightGIF
+
+	// WebGLDebugJs is the content of https://raw.githubusercontent.com/KhronosGroup/WebGLDeveloperTools/b42e702487d02d5278814e0fe2e2888d234893e6/src/debug/webgl-debug.js
+	WebGLDebugJs = webGLDebug
 )
