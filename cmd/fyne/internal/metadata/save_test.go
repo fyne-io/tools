4907285b35821361206e7883b2c6fa2c48a5a24c
Jacob Alzén <jacalz@tutanota.com>
2025-06-21 14:43:40 +0200

    Stop indenting FyneApp.toml (#62)

--- cmd/fyne/internal/metadata/save_test.go
+++ cmd/fyne/internal/metadata/save_test.go
@@ -1,7 +1,9 @@
 package metadata
 
 import (
+	"bytes"
 	"os"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -34,4 +36,22 @@ func TestSaveAppMetadata(t *testing.T) {
 	data2, err := Load(r)
 	assert.Nil(t, err)
 	assert.Equal(t, 2, data2.Details.Build)
+}
+
+func TestSaveIndentation(t *testing.T) {
+	r, err := os.Open("./testdata/FyneApp.toml")
+	assert.Nil(t, err)
+	data, err := Load(r)
+	assert.Nil(t, err)
+	r.Close()
+
+	w := &bytes.Buffer{}
+	err = Save(data, w)
+	assert.Nil(t, err)
+
+	content, err := os.ReadFile("./testdata/FyneApp.toml")
+	assert.Nil(t, err)
+	expected := strings.ReplaceAll(string(content), "\r\n", "\n")
+	actual := strings.ReplaceAll(w.String(), "\r\n", "\n")
+	assert.Equal(t, expected, actual)
 }
