48867682cbe49830f36db5142b656465ea52edcf
Jacalz <jacalz@tutanota.com>
2025-11-03 18:02:53 +0100

    Add back local LDFLAG lookup

--- cmd/fyne/internal/commands/build_test.go
+++ cmd/fyne/internal/commands/build_test.go
@@ -93,7 +93,7 @@ func Test_BuildLinuxReleaseVersion(t *testing.T) {
 		{
 			expectedValue: expectedValue{
 				args:  []string{"build", "-trimpath", "-ldflags", "-s -w", "-tags", "release", relativePath},
-				env:   []string{"CGO_ENABLED=1", "GOOS=linux", fmt.Sprintf("CGO_CFLAGS=%s%s %s%s", cflags, baseCFLAGSRelease, hardeningCFLAGS, archcflags), hardeningLDFLAGSLinux},
+				env:   []string{"CGO_ENABLED=1", "GOOS=linux", fmt.Sprintf("CGO_CFLAGS=%s%s %s%s", cflags, baseCFLAGSRelease, hardeningCFLAGS, archcflags), fmt.Sprintf("CGO_LDFLAGS=%s%s", ldflags, hardeningLDFLAGSLinux)},
 				osEnv: true,
 				dir:   "myTest",
 			},
