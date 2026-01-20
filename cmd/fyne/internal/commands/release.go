8acead2b9986a46a15f3495314e68309d08ca4f6
abakum <koka.abakum@gmail.com>
2026-01-20 21:14:28 +0300

    zipAlign, dex.go, arsc.go, findLast

--- cmd/fyne/internal/commands/release.go
+++ cmd/fyne/internal/commands/release.go
@@ -504,7 +504,7 @@ func (r *Releaser) zipAlign(path string) error {
 	}
 
 	cmd := filepath.Join(util.AndroidBuildToolsPath(), "zipalign")
-	err = exec.Command(cmd, "16", unaligned, path).Run()
+	err = exec.Command(cmd, "-p", "16", unaligned, path).Run()
 	if err != nil {
 		_ = os.Rename(path, unaligned) // ignore error, return previous
 		return err
