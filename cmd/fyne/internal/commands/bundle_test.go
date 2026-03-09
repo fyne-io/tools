9d5def8b8e2b16ea65da8b0c749e457f4fd5ae70
Andy Williams <andy@andy.xyz>
2026-03-09 16:48:20 +0000

    Bundle should be gofumpt approved

--- cmd/fyne/internal/commands/bundle_test.go
+++ cmd/fyne/internal/commands/bundle_test.go
@@ -43,7 +43,7 @@ func TestWriteResource(t *testing.T) {
 	writeHeader("test", f)
 	writeResource("testdata/bundle/content.txt", "contentTxt", f)
 
-	const header = fileHeader + "\n\npackage test\n\nimport (\n\t_ \"embed\"\n\t\"fyne.io/fyne/v2\"\n)\n\n"
+	const header = fileHeader + "\n\npackage test\n\nimport (\n\t_ \"embed\"\n\n\t\"fyne.io/fyne/v2\"\n)\n\n"
 	const expected = header + "//go:embed testdata/bundle/content.txt\nvar contentTxtData []byte\nvar contentTxt = &fyne.StaticResource{\n\tStaticName:    \"testdata/bundle/content.txt\",\n\tStaticContent: contentTxtData,\n}\n"
 
 	// Seek file to start so we can read the written data.
