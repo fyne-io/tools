edad7edfee939e83693e8d16ef3a3c02461a8489
Simon Dassow <simon@fabams.nl>
2026-07-21 11:26:45 +0200

    Remove -fcf-protection for arm64 in general (to address #150)

--- cmd/fyne/internal/commands/hardening_flags.go
+++ cmd/fyne/internal/commands/hardening_flags.go
@@ -22,7 +22,7 @@ type hardeningFlags struct {
 var hardeningFlagsTable = []hardeningFlags{
 	{"ubuntu", "amd64", "gcc", "*", "11.4.0", "-fcf-protection -fstack-protector-strong"},  // Ubuntu 22.04/gcc 11.4.0 fails with _FORTIFY_SOURCE redefined error
 	{"windows", "*", "gcc", "*", "*", "-D_FORTIFY_SOURCE=3 -fstack-protector-strong"},      // mingw doesn't support -fcf-protection -- XXX: double check for better conditions
-	{"darwin", "arm64", "clang", "*", "*", "-D_FORTIFY_SOURCE=3 -fstack-protector-strong"}, // -fcf-protection unsupported on this target
+	{"*", "arm64", "*", "*", "*", "-D_FORTIFY_SOURCE=3 -fstack-protector-strong"}, // -fcf-protection unsupported on arm64
 }
 
 func ccVersion() string {
